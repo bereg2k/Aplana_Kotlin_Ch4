2d447f44d9929e6255c217b08b67014fba1889ca
Oleg Berezhnoy <bereg2k@gmail.com>
2019-09-04 21:19:10 +0300

    Task 20

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,18 @@
-/** Задание 19: Авиакомпания 3. Наследование
- * Создадим наследника класса Aircraft с названием Boeing747 и добавим ему свойство, обозначающее вместимость пассажиров.
+/** Задание 20: Авиакомпания 4. Интерфейс
+ * Изменим реализацию пассажирских самолетов.
+ * Создадим Интерфейс с названием Passenger со свойством, обозначающим вместимость пассажиров.
+ * Уберем это свойство у класса Boeing747 и реализуем в нем новый интерфейс.
  */
 fun main(args: Array<String>) {
+    // объект класса в явном виде задающий количество мест в салоне
     val boeing747 = Boeing747(350)
     println(boeing747.info)
+
+    println()
+
+    // объект другого класса, использующий значение количества мест в самолете по умолчанию
+    val superJet = SuperJet()
+    println(superJet.info)
 }
 
 /**
@@ -19,16 +28,47 @@ open class Aircraft(private val maxFlightLength: Int, private val fuelTankCapaci
 
     val fuelConsumption: Double
         get() = fuelTankCapacity.toDouble() / maxFlightLength
-    val info: String = "Aircraft info:\n" +
+    open val info: String = "Aircraft info:\n" +
             "Maximum Flight Length = $maxFlightLength km\n" +
             "Fuel Tank Capacity = $fuelTankCapacity liters\n" +
-            "Fuel Consumption = $fuelConsumption L/km"
+            "Fuel Consumption = $fuelConsumption L/km\n"
+}
+
+/**
+ * Интерфейс для классов, описывающих самолеты со свойствами:
+ * - passengerSeatsNumber: количество мест в салоне самолета (имеет дефолтный геттер)
+ * - businessClassSeatsNumber: количество мест в салоне для пассажиров бизнес-класса (абстрактное)
+ */
+interface Passenger {
+    val passengerSeatsNumber: Int
+        get() = 250
+    val businessClassSeatsNumber: Int
 }
 
 /**
  * Класс-наследник от Aircraft для самолетов Boeing 747.
- * Определяет одно приватное поле passengerSeatsNumber - количество мест для пассажиров в салоне.
  * Первичный конструктор Boeing747 определен через вызов первичного конструктора Aircraft
  * с фиксированными значениями параметров maxFlightLength и fuelTankCapacity по умолчанию.
+
+ * Реализует интерфейс Passenger, при этом переопределяет свойство passengerSeatsNumber, предоставляя
+ * возможность вручную задать параметр при инициализации класса (игнорируя реализацию по умолчанию).
+ * Также класс вынужденно переопределяет свойство businessClassSeatsNumber (т.к. оно абстрактное в интерфейсе Passenger)
+ *
+ * Ещё класс переопределяет свойство info, фактически свойство родителя своей дополнительной инфой
  */
-class Boeing747(private val passengerSeatsNumber: Int) : Aircraft(10299, 300499)
+class Boeing747(seats: Int) : Aircraft(10299, 300499), Passenger {
+    override val passengerSeatsNumber: Int = seats
+    override val businessClassSeatsNumber: Int = seats / 10
+    override val info: String = super.info + "Number of passengers seats = $passengerSeatsNumber"
+}
+
+/**
+ * Еще один наследник Aircraft, также реализует интерфейс Passenger.
+ * Но при этом не переопределяет его свойство passengerSeatsNumber.
+ * При инициализации объекта данного класса значение свойства будет взято из геттера интерфейса Passenger.
+ * Вынужденно переопределяет свойство businessClassSeatsNumber путем присваивания константного значения по умолчанию.
+ */
+class SuperJet : Aircraft(), Passenger {
+    override val businessClassSeatsNumber: Int = 10
+    override val info: String = super.info + "Number of passengers seats = $passengerSeatsNumber"
+}
